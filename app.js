b5e115ac1f29ecaff3d63627df958922f6bce2bb
Paweł Niedźwiecki <hello@uxu.pl>
2021-06-01 12:14:55 +0200

    i added opction with display pug

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var mongoose = require("mongoose");
 var configDB = require("./config/db.config");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
 
 //Set up default mongoose connection
 var mongoDB = `mongodb+srv://${configDB.username}:${configDB.password}@${configDB.host}/${configDB.db}?retryWrites=true&w=majority`;
@@ -15,10 +21,9 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -28,5 +33,21 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 module.exports = app;
