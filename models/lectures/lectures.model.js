f8b710422976426515a01e7611f5f61fbb563241
Paweł Niedźwiecki <hello@uxu.pl>
2021-06-01 16:33:33 +0200

    i modified file with lectures schema

---
+++ models/lectures/lectures.model.js
@@ -0,0 +1,7 @@
+// import all function and pligins for lectures models
+const SchemaLecturesDB = require("./schemaDB/schema.db");
+
+// Export models
+module.exports = {
+  SchemaLecturesDB: SchemaLecturesDB,
+};
---
+++ models/lectures/schemaDB/schema.db.js
@@ -0,0 +1,40 @@
+// Dependencies import
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema for Lectures
+const schemaLectures = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  cover: { type: Schema.Types.ObjectId, ref: "file" },
+  section: [
+    {
+      _sectionID: Schema.Types.ObjectId,
+      sectionName: { type: String, required: true },
+      sectionDescription: { type: String, required: true },
+      sectionLesson: [
+        {
+          _lessonID: Schema.Types.ObjectId,
+          lessonName: { type: String, required: true },
+          lessonDescription: { type: String, required: true },
+          lessonVideo: {
+            type: Schema.Types.ObjectId,
+            ref: "file",
+            required: true,
+          },
+          lessonCover: {
+            type: Schema.Types.ObjectId,
+            ref: "file",
+            required: true,
+          },
+          lessonFile: [{ type: Schema.Types.ObjectId, ref: "file" }],
+        },
+      ],
+    },
+  ],
+  dateAdded: { type: Date, default: Date.now, required: true },
+  dateLastModified: { type: Date, default: Date.now, required: true },
+});
+
+// Export Schama and Create model for Lectures
+module.exports = mongoose.model("lecture", schemaLectures);
