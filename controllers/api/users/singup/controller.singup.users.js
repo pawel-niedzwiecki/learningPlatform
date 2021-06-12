a51b2e817aa7180254a5c2fb84a5ba87272bf18e
Paweł Niedźwiecki <hello@uxu.pl>
2021-06-12 13:58:09 +0200

    i added singup module

--- controllers/api/users/singup/controller.singup.users.js
+++ controllers/api/users/singup/controller.singup.users.js
@@ -8,7 +8,7 @@ exports.users_singup_post = async function (req, res, next) {
   if (!req.body.email || !req.body.password)
     return res
       .status(400)
-      .json({ success: false, require: "email & password" });
+      .json({ success: false, message: "email & password is require" });
 
   // i try create new user
   await new usersModules({
@@ -17,12 +17,15 @@ exports.users_singup_post = async function (req, res, next) {
   }).save(function (err) {
     if (err) {
       if (err.errors)
-        return res
-          .status(400)
-          .json({ success: false, email: err.errors.email.properties.message });
+        return res.status(400).json({
+          success: false,
+          message: err.errors.email.properties.message,
+        });
       if (err.keyPattern.email === 1 && err.code === 11000)
         // i send mesage when is not unique email
-        return res.status(400).json({ success: false, email: "not is unique" });
+        return res
+          .status(400)
+          .json({ success: false, message: "not is unique" });
     }
 
     // i send mesage when i finish create new user
